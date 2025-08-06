a1548d08c1fb67b074a7c3540d4c40648c2c174a
0xKolyanus <mukola.1zt.@gmail.com>
2025-08-06 13:41:16 +0300

    added a dashboard url to view

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,12 @@
 Rails.application.routes.draw do
-  get "dashboard/index"
+  get "/dashboard" => "dashboard#index", as: :dashboard
   get "dashboard/properties"
   get "dashboard/reports"
+
   resources :properties
   devise_for :accounts
   root to: "public#main"
   get "public/main"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
